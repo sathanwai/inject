02cd4e78abfdae6fa1240e5d98d1907b41138da1
kuroshibin <hahahathai@gmail.com>
2018-05-22 00:18:45 -0700

    Update main.py

--- main.py
+++ main.py
@@ -116,7 +116,7 @@ class Server:
         time.sleep(1)
         sys.stderr.write(self.config('Configuration'))
         time.sleep(2)
-        sys.stderr.write(self.log('ต่อเเล้วเรียบร้อยจ้ะ ไอ้หำ'))
+        sys.stderr.write(self.log('========Inject Sukses======== \nReading Server:'))
 
     def run(self):
         ServerHandler.LogWindow(True)
